9e19dfc86e59597404cf81cb10f52f5e9f507c73
Evelina Söderberg <en222dc@gmail.com>
2020-10-26 14:35:14 +0100

    Added BaseUrl for Api´s Created repositories and mockdata

---
+++ interaktiva20_2/Data/IOMDbRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace interaktiva20_2.Data
+{
+    interface IOMDbRepository
+    {
+    }
+}
---
+++ interaktiva20_2/Data/OMDbRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace interaktiva20_2.Data
+{
+    public class OMDbRepository : IOMDbRepository
+    {
+
+    }
+}
