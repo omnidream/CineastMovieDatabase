bef3433a1172531772ea10e4eead755a4234a541
Evelina Söderberg <en222dc@gmail.com>
2020-10-28 19:36:20 +0100

    Added properties to MovieViewModel

--- interaktiva20_2/Models/ViewModels/MovieViewModel.cs
+++ interaktiva20_2/Models/ViewModels/MovieViewModel.cs
@@ -5,14 +5,29 @@ using System.Threading.Tasks;
 
 namespace interaktiva20_2.Models.ViewModels
 {
-    public static class MovieViewModel
+    public class MovieViewModel
     {
         //FÖR VARJE LISTTYP
         //Hämta från CMDB till en lista
         //Lägg i TASK-lista
         //Hämta från OMDB där ID stämmer i en foreach typ
 
-        //Skicka retur IEnumerable-List med Lists tillbaka till View
+        //Skicka retur IEnumerable-List med Lists tillbaka till View 
+                        //=> MÅSTE vi det? Eller kan vi bara låta konstruktorn sätta värden till propparna så vi kommer åt dem?
+
+
+        //Dessa proppar bör vi ha med eftersom det är dem vi har med i Index
+        public int MovieNumber { get; set; } //Finns inte med från api men vi vill visa
+        public string Title { get; set; }
+        public string Poster { get; set; }
+        public string ImdbRating { get; set; }
+        public string Plot { get; set; }
+
+
+        public MovieViewModel()
+        {
+
+        }
 
 
     }
