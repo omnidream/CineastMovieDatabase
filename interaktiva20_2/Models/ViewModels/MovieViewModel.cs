0e18397c80dcdd6a39836b823ea13a6f69ebaa74
Gustav Kron <gustav@vindsrummet.com>
2020-10-28 14:38:42 +0100

    Added a nice comment, almost like a TODO

--- interaktiva20_2/Models/ViewModels/MovieViewModel.cs
+++ interaktiva20_2/Models/ViewModels/MovieViewModel.cs
@@ -7,8 +7,13 @@ namespace interaktiva20_2.Models.ViewModels
 {
     public static class MovieViewModel
     {
-        
-            
+        //FÖR VARJE LISTTYP
+        //Hämta från CMDB till en lista
+        //Lägg i TASK-lista
+        //Hämta från OMDB där ID stämmer i en foreach typ
+
+        //Skicka retur IEnumerable-List med Lists tillbaka till View
+
 
     }
 }
