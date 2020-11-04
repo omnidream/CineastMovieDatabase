348696019bb67fbdd414b3a7163c03cb2b825540
Evelina Söderberg <en222dc@gmail.com>
2020-11-04 09:36:50 +0100

    Added class .sticky to search bar to be added on scroll Managed CSS and JS-files and changed names Changed @section "MyStylesheets" to @section "MyAssets" and added specific JS when needed

---
+++ interaktiva20_2/wwwroot/assets/js/JavaScriptHome.js
@@ -0,0 +1,51 @@
+﻿/*READ MORE PLOT*/
+let readMoreText = document.querySelectorAll('.moviePlot');
+let link = document.querySelector('.more');
+console.log(link);
+let numberOfShownChars = 130;
+
+readMoreText.forEach(plot => {
+    let shownText = plot.innerHTML.slice(0, numberOfShownChars);
+    let hiddenText = plot.innerHTML.slice(numberOfShownChars);
+    plot.innerHTML = shownText + '<span class="moreDots"> ...</span> <span class="trimmedText">' + hiddenText + '</span>';
+})
+
+document.onclick = function (event) {
+    if (event.target.className === 'more') {
+        event.target.parentElement.parentElement.classList.toggle('showAll');
+    }
+}
+
+/* LAYOUT HOME INDEX */
+//TODO: PRIO 2 Korta ner Layout Home Index
+let topRatedList = document.querySelector('.topRatedList');
+let mostPopularList = document.querySelector('.mostPopularList');
+let neverRatedList = document.querySelector('.neverRatedList');
+
+let divTopRated = document.querySelector('#topFlex');
+let divMostPopular = document.querySelector('#mostPopularFlex');
+let divNeverRated = document.querySelector('#neverRatedFlex');
+
+let topRatedMovies = Array.from(topRatedList.children);
+let mostPopularMovies = Array.from(mostPopularList.children);
+let neverRatedMovies = Array.from(neverRatedList.children);
+
+topRatedMovies[0].classList.add('topMovie');
+mostPopularMovies[0].classList.add('topMovie');
+neverRatedMovies[0].classList.add('topMovie');
+
+
+topRatedMovies.slice(1).forEach(movie => {
+    movie.classList.add('movieList');
+    divTopRated.appendChild(movie);
+});
+
+mostPopularMovies.slice(1).forEach(movie => {
+    movie.classList.add('movieList');
+    divMostPopular.appendChild(movie);
+});
+
+neverRatedMovies.slice(1).forEach(movie => {
+    movie.classList.add('movieList');
+    divNeverRated.appendChild(movie);
+});
