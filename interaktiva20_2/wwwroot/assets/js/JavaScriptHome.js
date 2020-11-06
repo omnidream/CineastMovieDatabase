fb7a9ac5704ba167926e1da5978dff30cced7bc0
Evelina Söderberg <en222dc@gmail.com>
2020-11-06 08:34:49 +0100

    Fixed the JS for showing the first movie in all lists different on Home Index

--- interaktiva20_2/wwwroot/assets/js/JavaScriptHome.js
+++ interaktiva20_2/wwwroot/assets/js/JavaScriptHome.js
@@ -16,35 +16,7 @@ document.onclick = function (event) {
 }
 
 /* LAYOUT HOME INDEX */
-//TODO: PRIO 2 Korta ner Layout Home Index
-let myMovieList = document.querySelector('.topList');
-//let mostPopularList = document.querySelector('.mostPopularList');
-//let neverRatedList = document.querySelector('.neverRatedList');
-
-//let divTopRated = document.querySelector('#topFlex');
-//let divMostPopular = document.querySelector('#mostPopularFlex');
-//let divNeverRated = document.querySelector('#neverRatedFlex');
-
-let topMovies = Array.from(myMovieList.children);
-//let mostPopularMovies = Array.from(mostPopularList.children);
-//let neverRatedMovies = Array.from(neverRatedList.children);
-
-topMovies[0].classList.add('topMovie');
-//mostPopularMovies[0].classList.add('topMovie');
-//neverRatedMovies[0].classList.add('topMovie');
-
-
-//topRatedMovies.slice(1).forEach(movie => {
-//    movie.classList.add('movieList');
-//    divTopRated.appendChild(movie);
-//});
-
-//mostPopularMovies.slice(1).forEach(movie => {
-//    movie.classList.add('movieList');
-//    divMostPopular.appendChild(movie);
-//});
-
-//neverRatedMovies.slice(1).forEach(movie => {
-//    movie.classList.add('movieList');
-//    divNeverRated.appendChild(movie);
-//});
+let myMovieList = document.querySelectorAll('.topList');
+myMovieList.forEach(list => {
+    list.firstElementChild.classList.add('topMovie');
+})
