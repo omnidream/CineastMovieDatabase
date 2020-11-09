1adf99ea7578f5ed12de890bee01bfefce11e704
Evelina Söderberg <en222dc@gmail.com>
2020-11-09 09:59:54 +0100

    Added SearchResultViewModel

---
+++ interaktiva20_2/Models/ViewModels/SearchResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿using interaktiva20_2.Models.DTO;
+using System.Collections.Generic;
+
+namespace interaktiva20_2.Models.ViewModels
+{
+    public class SearchResultViewModel : ISearchResultDto
+    {
+        public string Response { get; set; }
+        public string Error { get; set; }
+        public string totalResults { get; set; }
+        public List<MovieDetailsDto> Search { get; set; }
+    }
+}
